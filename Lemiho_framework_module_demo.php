e7946cc5bcc4b69273be81f4a803157e3761f36f
unknown <lengochoang3112@gmail.com>
2017-12-28 22:14:47 +0700

    update

---
+++ Lemiho_framework_module_demo.php
@@ -0,0 +1,8 @@
+<?php Lemiho_framework_module_demo extends SYSTEM_LEMIHO {
+                      public function __construct(){
+                            parent::__construct();
+                        }
+                      public function indexAction(){
+                          echo "Hello Demo module";
+                      }  
+}
---
+++ model/Lemiho_framework_module_demo_model.php
@@ -0,0 +1,5 @@
+<?php Lemiho_framework_module_demo extends SYSTEM_LEMIHO_MODEL {
+                  public function __construct(){
+                      parent::__construct();
+                  }
+}
