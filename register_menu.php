a180da52b4799da379fe57410132495f95445e0b
unknown <lengochoang3112@gmail.com>
2017-12-29 18:51:08 +0700

    Update

---
+++ register_menu.php
@@ -0,0 +1,15 @@
+<?php $menu = array(
+              'name' => 'Lemiho_framework_module_demo',
+              'url'  => 'demo/manage',
+              'icon' => 'glyphicon glyphicon-home',
+              'submenu' => array(
+                    array(
+                        'name' => 'submenu 1',
+                        'url'  => 'demo/sub1'
+                    ),
+                    array(
+                        'name' => 'submenu 2',
+                        'url'  => 'demo/sub2'
+                    )
+              )
+);
